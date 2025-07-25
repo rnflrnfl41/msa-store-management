4b37eb92207739aa63da097d6446a1a4439b49d4
bin <rnflrnfl41@naver.com>
2025-07-25 23:59:02 +0900

    initial commit

---
+++ customer-service/src/main/java/com/example/customerservice/CustomerController.java
@@ -0,0 +1,16 @@
+package com.example.customerservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+    @GetMapping
+    public List<String> getCustomers() {
+        return List.of("고객A", "고객B");
+    }
+}
