c9578f1e12de9f3133292d3809d3e4bc5b96d743
bin <rnflrnfl41@naver.com>
2025-07-28 01:04:08 +0900

    feat: 공통 api response 추가

---
+++ auth-service/src/main/java/com/example/authservice/util/ApiResponse.java
@@ -0,0 +1,27 @@
+package com.example.authservice.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private int code;
+    private String message;
+    private T data;
+    private LocalDateTime timestamp;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(200,
+                "요청이 성공적으로 처리되었습니다.",
+                data,LocalDateTime.now());
+    }
+
+}
