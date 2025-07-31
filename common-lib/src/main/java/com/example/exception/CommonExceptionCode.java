43bca56389326c1145d887ea8bd66775ca49b9d1
bin <rnflrnfl41@naver.com>
2025-07-31 15:42:39 +0900

    feat: 필수 파라미터가 안들어왔을때 공통 에러처리 로직

--- common-lib/src/main/java/com/example/exception/CommonExceptionCode.java
+++ common-lib/src/main/java/com/example/exception/CommonExceptionCode.java
@@ -8,9 +8,16 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum CommonExceptionCode {
 
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰 입니다."),
+    MISSING_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 없습니다."),
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료 되었습니다."),
+
+
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
-    ID_PASSWORD_FAIL(HttpStatus.BAD_REQUEST, "비밀번호가 올바르지않습니다.");
+    ID_PASSWORD_FAIL(HttpStatus.BAD_REQUEST, "비밀번호가 올바르지않습니다."),
+
+
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "파라미터가 올바르지않습니다.");
 
     private final HttpStatus status;
     private final String message;
