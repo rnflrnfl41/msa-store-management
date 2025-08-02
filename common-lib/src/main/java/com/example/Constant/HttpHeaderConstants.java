ec13ced8dd9eea7f375277bcc46a546ac0c096a4
bin <rnflrnfl41@naver.com>
2025-08-02 19:35:12 +0900

    feat: 토큰에 넣을 사용자 정보 상수화

---
+++ common-lib/src/main/java/com/example/Constant/HttpHeaderConstants.java
@@ -0,0 +1,12 @@
+package com.example.Constant;
+
+public class HttpHeaderConstants {
+    public static final String X_USER_IDX = "X-USER-IDX";
+    public static final String X_USER_LOGIN_ID = "X-USER-LOGIN-ID";
+    public static final String X_USER_NAME = "X-USER-NAME";
+    public static final String X_USER_STORE_ID = "X-USER-STORE-ID";
+    public static final String X_GATEWAY_TOKEN = "X-GATEWAY-TOKEN";
+
+    public static final String X_USER_ROLE = "X-USER-ROLE";
+
+}
