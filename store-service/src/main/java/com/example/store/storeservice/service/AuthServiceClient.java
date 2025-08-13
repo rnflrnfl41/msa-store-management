0aeb8fddece25b4007b07d3f14e4a51efe341194
bin <rnflrnfl41@naver.com>
2025-08-13 17:23:10 +0900

    feat: store-service AuthServiceClient.java 추가

--- store-service/src/main/java/com/example/store/storeservice/service/AuthServiceClient.java
+++ store-service/src/main/java/com/example/store/storeservice/service/AuthServiceClient.java
@@ -1,27 +1,50 @@
 package com.example.store.storeservice.service;
 
+
+import com.example.dto.UserDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import com.example.dto.ApiResponse;
+
+import java.util.List;
+import java.util.UUID;
+
+
+import static com.example.Constant.HttpHeaderConstants.X_USER_ROLE;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class AuthServiceClient {
 
-    private final WebClient.Builder webClientBuilder;
+    private final WebClient userServiceWebClient;
+
+    public List<UserDto> getAllUserInfoByStoreId(UUID storeId, String userRole) {
+
+        return userServiceWebClient
+                .get()
+                .uri("/{storeId}", storeId)
+                .header(X_USER_ROLE, userRole)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<ApiResponse<List<UserDto>>>() {})
+                .map(ApiResponse::getData)
+                .block();
+    }
 
     public ResponseEntity<Void> deleteUserByStoreId(int storeId, String internalToken, String userId, String userRole) {
-        return webClientBuilder
-                 .build()
+        return userServiceWebClient
                 .delete()
-                .uri("http://auth-service/storeId/" + storeId)
-                .header("X-INTERNAL-TOKEN", internalToken)
+                .uri("/store/{storeId}" + storeId)
                 .header("X-USER-ROLE", userRole)
                 .retrieve()
                 .toBodilessEntity()
                 .block(); // 동기 방식. 필요 시 비동기로 처리 가능
     }
+
 
 
 }
