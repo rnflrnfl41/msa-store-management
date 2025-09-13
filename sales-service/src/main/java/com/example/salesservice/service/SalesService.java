0c043894c784d8bd5bc9a5876ee8a834cd34a36c
rnflrnfl41 <120080932+rnflrnfl41@users.noreply.github.com>
2025-09-13 17:20:49 +0900

    feat: 매출 저장 api 추가

--- sales-service/src/main/java/com/example/salesservice/service/SalesService.java
+++ sales-service/src/main/java/com/example/salesservice/service/SalesService.java
@@ -1,11 +1,15 @@
 package com.example.salesservice.service;
 
+import com.example.salesservice.dto.SalesRegistrationDto;
 import com.example.salesservice.dto.ServiceHistoryDto;
 import com.example.salesservice.dto.ServiceItemDto;
+import com.example.salesservice.entity.Payment;
+import com.example.salesservice.entity.ServiceItem;
 import com.example.salesservice.entity.Visit;
 import com.example.salesservice.repository.PaymentRepository;
 import com.example.salesservice.repository.ServiceItemRepository;
 import com.example.salesservice.repository.VisitRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -42,5 +46,32 @@ public class SalesService {
                             .build();
                 })
                 .toList();
+    }
+
+    @Transactional
+    public void registerSales(SalesRegistrationDto registrationDto, Integer storeId) {
+
+        Visit visit = modelMapper.map(registrationDto, Visit.class);
+        visit.setStoreId(storeId);
+
+        visitRepository.save(visit);
+
+        paymentRepository.save(Payment.builder()
+                .amount(registrationDto.getFinalServiceAmount())
+                .discount(registrationDto.getDiscountAmount())
+                .paymentMethod(registrationDto.getPaymentMethod())
+                .pointsUsed(registrationDto.getUsedPoint())
+                .visit(visit)
+                .build()
+        );
+
+        registrationDto.getServiceList().stream().map(s -> ServiceItem.builder()
+                .serviceName(s.getName())
+                .price(s.getPrice())
+                .visit(visit)
+                .build()
+        ).forEach(serviceItemRepository::save);
+
+
     }
 }
