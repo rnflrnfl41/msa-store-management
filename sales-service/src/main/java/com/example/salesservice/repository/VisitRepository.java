db1d8ce9bedbdc95583a124c9acf3446bb2d49f9
rnflrnfl41 <120080932+rnflrnfl41@users.noreply.github.com>
2025-09-17 23:44:15 +0900

    feat: 월별 차트 및 일별 차트 불러오는 쿼리 추가

--- sales-service/src/main/java/com/example/salesservice/repository/VisitRepository.java
+++ sales-service/src/main/java/com/example/salesservice/repository/VisitRepository.java
@@ -1,5 +1,6 @@
 package com.example.salesservice.repository;
 
+import com.example.salesservice.dto.ChartData;
 import com.example.salesservice.dto.SalesSummary;
 import com.example.salesservice.entity.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -23,5 +24,29 @@ public interface VisitRepository extends JpaRepository <Visit, Integer> {
             ")" +
             " from Visit v where month(v.visitDate) = :month and v.storeId = :storeId ")
     SalesSummary getSummarySalesMonth(int month, int storeId);
+
+    // 월별 차트 데이터
+    @Query("select new com.example.salesservice.dto.ChartData(" +
+            "v.visitDate, " +
+            "COALESCE(sum(v.finalServiceAmount),0), " +
+            "count(v)" +
+            ")" +
+            " from Visit v where v.storeId = :storeId and " +
+            " v.visitDate between :startDate and :endDate " +
+            " group by year(v.visitDate), month(v.visitDate) " +
+            " order by year(v.visitDate), month(v.visitDate)")
+    List<ChartData> getMonthlyChartDataByPeriod(LocalDate startDate, LocalDate endDate, int storeId);
+
+    // 일별 차트 데이터
+    @Query("select new com.example.salesservice.dto.ChartData(" +
+            "v.visitDate, " +
+            "COALESCE(sum(v.finalServiceAmount),0), " +
+            "count(v)" +
+            ")" +
+            " from Visit v where v.storeId = :storeId and " +
+            " v.visitDate between :startDate and :endDate " +
+            " group by v.visitDate " +
+            " order by v.visitDate")
+    List<ChartData> getDailyChartDataByPeriod(LocalDate startDate, LocalDate endDate, int storeId);
 
 }
