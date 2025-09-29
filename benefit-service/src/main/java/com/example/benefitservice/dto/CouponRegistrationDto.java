6a132a9c8701ad6cd74f28c21a78e4a60b407d88
rnflrnfl41 <120080932+rnflrnfl41@users.noreply.github.com>
2025-09-29 21:50:03 +0900

    fix: CouponRegistrationDto setter, getter, AllArgsConstructor 추가

--- benefit-service/src/main/java/com/example/benefitservice/dto/CouponRegistrationDto.java
+++ benefit-service/src/main/java/com/example/benefitservice/dto/CouponRegistrationDto.java
@@ -1,7 +1,14 @@
 package com.example.benefitservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Setter
+@Getter
+@AllArgsConstructor
 public class CouponRegistrationDto {
 
     private String name;
